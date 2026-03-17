42cfd1fb20dd1f796e6467adf188a827a506b036
Kate Brownbill <glowkate@gmail.com>
2026-03-17 14:42:39 -0400

    Added places for future functions to go.

--- src/cps406_bugtracker/Buglist.java
+++ src/cps406_bugtracker/Buglist.java
@@ -16,10 +16,34 @@ public class Buglist {
 	}
 	
 	/*  
-	 *  Returns 0 on successful addition
-	 *  Returns 1 on failed addition
+	 * Bug must have a unique name
+	 *  
+	 * Returns 0 on successful addition
+	 * Returns 1 on failed addition for unknown reason
+	 * Returns 2 on failed addition due to duplicate bug name
 	 */
-	public int addBug(Bug addedBug) {
+	public int addBug(String bugName, String bugDescription) {
 		return (1);
+	}
+	
+	private int addBug(Bug addedBug) {
+		if(findBug(addedBug.getName()) != -1) {
+			return(2);
+		}
+		return (1);
+	}
+	
+	/*
+	 * Returns index of given bug. If bug wasn't found, returns -1
+	 */
+	private int findBug(String bugName) {
+		Bug b;
+		for(int i = 0; i < buglist.size(); i++) {
+			b = buglist.get(i);
+			if(b.getName().equals(bugName)) {
+				return(i);
+			}
+		}
+		return(-1);
 	}
 }
