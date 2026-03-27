842f9f09a3e96c49d82c416682f8797837185d05
Kate Brownbill <glowkate@gmail.com>
2026-03-27 14:59:31 -0400

    Progressed working on buglist

--- src/cps406_bugtracker/Buglist.java
+++ src/cps406_bugtracker/Buglist.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 public class Buglist {
 	
 	private ArrayList<Bug> buglist;
+	private int nextID;
 	
 	public Buglist(){
 		buglist = new ArrayList<Bug>();
+		nextID = 0;
 	}
 	
 	// TODO: Add code for copying the contents of the copylist (MAKE NEW BUGS, DON'T JUST GRAB THE POINTERS)
 	public Buglist(Buglist copylist){
 		buglist = new ArrayList<Bug>();
+		nextID = copylist.getNextID();
 		for (Bug b : copylist.getBugArrayList()) {
 			buglist.add(new Bug(b));
 		}
@@ -22,32 +25,32 @@ public class Buglist {
 		return (buglist);
 	}
 	
-	/*  
-	 * Bug must have a unique name
-	 *  
-	 * Returns 0 on successful addition
-	 * Returns 1 on failed addition for unknown reason
-	 * Returns 2 on failed addition due to duplicate bug name
-	 */
-	public int addBug(String bugName, String bugDescription) {
-		return (1);
+	private int newID() {
+		int newID = nextID;
+		nextID += 1;
+		return(newID);
 	}
 	
-	private int addBug(Bug addedBug) {
-		if(findBug(addedBug.getName()) != -1) {
-			return(2);
-		}
-		return (1);
+	private int getNextID() {
+		return (nextID);
 	}
 	
-	/*
-	 * Returns index of given bug. If bug wasn't found, returns -1
-	 */
-	private int findBug(String bugName) {
-		Bug b;
+	public void addBug(String bugName, String bugDescription) {
+		Bug newBug = new Bug(bugName, bugDescription, newID());
+		buglist.add(newBug);
+	}
+	
+	
+	private void addBug(Bug addedBug) {
+		Bug newBug = new Bug(addedBug);
+		buglist.add(newBug);
+	}
+	
+	private int getBugIndexFromID(int fetchID) {
+		Bug curBug;
 		for(int i = 0; i < buglist.size(); i++) {
-			b = buglist.get(i);
-			if(b.getName().equals(bugName)) {
+			curBug = buglist.get(i);
+			if(curBug.getID() == fetchID) {
 				return(i);
 			}
 		}
